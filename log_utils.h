5e8b75a14308c3ddca99a9d5efb3e86d364f38b1
p0w3r-sav3r-m0de <111459558+ring0-c0d3-br34k3r@users.noreply.github.com>
2024-09-17 13:23:20 +0100

    instead of PrintDbg or PrintDbgEx i do log file

---
+++ log_utils.h
@@ -0,0 +1,10 @@
+#ifndef _LOG_UTILS_H_
+#define _LOG_UTILS_H_
+
+#include <ntddk.h>
+
+VOID InitializeLogFile(VOID);
+VOID LogMessage(const char* format, ...);
+VOID CloseLogFile(VOID);
+
+#endif // _LOG_UTILS_H_
